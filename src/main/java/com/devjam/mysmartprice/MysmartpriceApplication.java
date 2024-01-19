3d82d214f163896f4c8c6da7153203afd687909e
vishakhamangtani <vishakhamangtani23@gmail.com>
2024-01-19 16:08:53 +0530

    Initial Commit

---
+++ src/main/java/com/devjam/mysmartprice/MysmartpriceApplication.java
@@ -0,0 +1,13 @@
+package com.devjam.mysmartprice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysmartpriceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysmartpriceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devjam/mysmartprice/MysmartpriceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devjam.mysmartprice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysmartpriceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
